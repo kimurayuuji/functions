e27d593be656d891ab6a71d80bf9a1f1823c9732
Yuji Kimura <kimurayuuji79@gmail.com>
2026-04-22 09:25:16 +0900

    modify

---
+++ ghq-fzf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ghq
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
